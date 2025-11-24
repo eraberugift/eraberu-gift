8380e771504bfccc708fe8719aa78cc5de327b6f
eraberugift <eraberugift@gmail.com>
2025-11-24 18:16:31 +0900

    Delete public directory

--- public/js/dotlottie/dotlottie-wc.js
+++
@@ -1,2 +0,0 @@
-export{a as DotLottieWC}from'./chunk-KZJ357XM.js';import'./chunk-O5GEFJ54.js';import'./chunk-2KOBN6OY.js';//# sourceMappingURL=dotlottie-wc.js.map
-//# sourceMappingURL=dotlottie-wc.js.map
